5a579a0c67f5306737c640a9ad6402e528c9908b
tamaki annen <tmn.agenkir.luck@icloud.com>
2021-05-14 22:39:41 +0900

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by 安念　玉希 on 2021/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
